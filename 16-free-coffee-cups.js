ccd96fff7f37a6d813b5c63ec69ecd5e916d45ca
xaviguasch <xaviguasch@gmail.com>
2020-06-13 19:21:18 +0200

    added alternative better solution for: Free Coffee Cups, VE level

--- 16-free-coffee-cups.js
+++ 16-free-coffee-cups.js
@@ -1,13 +1,15 @@
 function totalCups(n) {
-  let extraCups = 0
+  // let extraCups = 0
 
-  if (n % 6 === 0) {
-    extraCups = n / 6
-  } else {
-    extraCups = Math.floor(n / 6)
-  }
+  // if (n % 6 === 0) {
+  //   extraCups = n / 6
+  // } else {
+  //   extraCups = Math.floor(n / 6)
+  // }
 
-  return n + extraCups
+  // return n + extraCups
+
+  return n + Math.floor(n / 6)
 }
 
 console.log(totalCups(6))
